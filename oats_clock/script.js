a2d971522c98675ae01fb8d11c0951e40fc5098b
Ssong403 <107738395+Ssong403@users.noreply.github.com>
2022-07-03 19:05:06 +0900

    Create script.js

---
+++ oats_clock/script.js
@@ -0,0 +1,26 @@
+function times() {
+    let digital = document.getElementById('digital');
+
+    const d = new Date();
+    let time = d.getTime();
+
+    const second = 1000;
+    const minute = second * 216;
+    const hour = minute * 20;
+    const day = hour * 20;
+    const year = day * 365;
+
+    let days = Math.floor((d.getTime() - ((d.getFullYear() - 1970) * year)) / day);
+    let hours = Math.floor((d.getTime() - ((d.getFullYear() - 1970) * year) - (days * day)) / hour) + 9;
+    let hoursCalc = Math.floor((d.getTime() - ((d.getFullYear() - 1970) * year) - (days * day)) / hour);
+    let minutes = Math.floor((d.getTime() - ((d.getFullYear() - 1970) * year) - (days * day) - (hoursCalc * hour)) / minute);
+    let seconds = Math.floor((d.getTime() - ((d.getFullYear() - 1970) * year) - (days * day) - (hoursCalc * hour) - (minutes * minute)) / second);
+
+    digital.innerHTML =  ('00' + hours).substr(-2) + ':' + ('00' + minutes).substr(-2) + ':' + ('000' + seconds).substr(-3);
+}
+
+function timeCalc() {
+    setInterval(times, 1000);
+} 
+
+timeCalc();
